1a61843289abc5e3f1dc936a4f60a0914b166efe
Rachit-Baranwal <rachitbarnwal038@gmail.com>
2024-03-23 17:44:28 +0530

    Integrated Weather APIs

--- src/main/java/com/forecast/api/controller/ForecastController.java
+++ src/main/java/com/forecast/api/controller/ForecastController.java
@@ -14,33 +14,47 @@ public class ForecastController {
 
     @Autowired
     Environment env;
+    
     @GetMapping(value = "/forecastByName/{cityName}")
-    public Response forecastSummary(@RequestHeader String clientId, @RequestHeader String clientSecret, @PathVariable String cityName) throws IOException {
+    public Response forecastSummary(
+    		@RequestHeader String clientId, 
+    		@RequestHeader String clientSecret,
+    		@PathVariable String cityName) throws IOException {
+    	 
+        String url=env.getProperty("URL")+cityName+"/summary/";
+        
         OkHttpClient client = new OkHttpClient();
-
         Request request = new Request.Builder()
-                .url(env.getProperty("URL")+cityName+"/summary/")
+                .url(url)
                 .get()
                 .addHeader("X-RapidAPI-Key", env.getProperty("KEY"))
                 .addHeader("X-RapidAPI-Host", env.getProperty("HOST"))
                 .build();
-
-        return client.newCall(request).execute();
+        
+        Response response = client.newCall(request).execute();
+        System.err.println(response);
+        return response;
     }
 
     @GetMapping(value = "/forecastByNameHourly/{cityName}")
-    public Response forecastSummaryHourly(@RequestHeader String clientId, @RequestHeader String clientSecret, @PathVariable String cityName) throws IOException {
+    public Response forecastSummaryHourly(
+    		@RequestHeader String clientId, 
+    		@RequestHeader String clientSecret, 
+    		@PathVariable String cityName) throws IOException {
+        
+        String url=env.getProperty("URL")+cityName+"/hourly/";
+       
         OkHttpClient client = new OkHttpClient();
-
         Request request = new Request.Builder()
-                .url(env.getProperty("URL")+cityName+"/hourly/")
+        		.url(url)
                 .get()
                 .addHeader("X-RapidAPI-Key", env.getProperty("KEY"))
                 .addHeader("X-RapidAPI-Host", env.getProperty("HOST"))
                 .build();
 
         Response response = client.newCall(request).execute();
-
-        return client.newCall(request).execute();
+        System.err.println(response);
+        
+        return response;
     }
 }
